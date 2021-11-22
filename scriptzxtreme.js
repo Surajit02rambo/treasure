adcc6a760e42cf487d79a0ce2ae5b1b0e86f3984
Surajit02rambo <82580421+Surajit02rambo@users.noreply.github.com>
2021-11-22 20:33:48 +0530

    added a file

---
+++ scriptzxtreme.js
@@ -0,0 +1,174 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "210px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.fontSize = "10px"
+  }
+
+
+  function openNav1() {
+    document.getElementById("mySidenav1").style.width = "210px";
+  }
+  
+  function closeNav1() {
+    document.getElementById("mySidenav1").style.width = "0";
+    document.getElementById("mySidenav1").style.fontSize = "10px"
+  }
+
+  function openNav2() {
+    document.getElementById("mySidenav2").style.width = "210px";
+  }
+  
+  function closeNav2() {
+    document.getElementById("mySidenav2").style.width = "0";
+    document.getElementById("mySidenav2").style.fontSize = "10px"
+  }
+
+
+  function openNav3() {
+    document.getElementById("mySidenav3").style.width = "210px";
+  }
+  
+  function closeNav3() {
+    document.getElementById("mySidenav3").style.width = "0";
+    document.getElementById("mySidenav3").style.fontSize = "10px"
+  }
+
+
+  function openNav4() {
+    document.getElementById("mySidenav4").style.width = "210px";
+  }
+  
+  function closeNav4() {
+    document.getElementById("mySidenav4").style.width = "0";
+    document.getElementById("mySidenav4").style.fontSize = "10px"
+  }
+
+  function openNav5() {
+    document.getElementById("mySidenav5").style.width = "210px";
+  }
+  
+  function closeNav5() {
+    document.getElementById("mySidenav5").style.width = "0";
+    document.getElementById("mySidenav5").style.fontSize = "10px"
+  }
+
+
+  function openNav6() {
+    document.getElementById("mySidenav6").style.width = "210px";
+  }
+  
+  function closeNav6() {
+    document.getElementById("mySidenav6").style.width = "0";
+    document.getElementById("mySidenav6").style.fontSize = "10px"
+  }
+
+
+  function openNav7() {
+    document.getElementById("mySidenav7").style.width = "210px";
+  }
+  
+  function closeNav7() {
+    document.getElementById("mySidenav7").style.width = "0";
+    document.getElementById("mySidenav7").style.fontSize = "10px"
+  }
+
+
+  function openNav8() {
+    document.getElementById("mySidenav8").style.width = "210px";
+  }
+  
+  function closeNav8() {
+    document.getElementById("mySidenav8").style.width = "0";
+    document.getElementById("mySidenav8").style.fontSize = "10px"
+  }
+
+  function openNav9() {
+    document.getElementById("mySidenav9").style.width = "210px";
+  }
+  
+  function closeNav9() {
+    document.getElementById("mySidenav9").style.width = "0";
+    document.getElementById("mySidenav9").style.fontSize = "10px"
+  }
+
+
+  function openNav10() {
+    document.getElementById("mySidenav10").style.width = "210px";
+  }
+  
+  function closeNav10() {
+    document.getElementById("mySidenav10").style.width = "0";
+    document.getElementById("mySidenav10").style.fontSize = "10px"
+  }
+
+
+  function openNav11() {
+    document.getElementById("mySidenav11").style.width = "210px";
+  }
+  
+  function closeNav11() {
+    document.getElementById("mySidenav11").style.width = "0";
+    document.getElementById("mySidenav11").style.fontSize = "10px"
+  }
+
+  function openNav12() {
+    document.getElementById("mySidenav12").style.width = "210px";
+  }
+  
+  function closeNav12() {
+    document.getElementById("mySidenav12").style.width = "0";
+    document.getElementById("mySidenav12").style.fontSize = "10px"
+  }
+
+
+  function openNav13() {
+    document.getElementById("mySidenav13").style.width = "210px";
+  }
+  
+  function closeNav13() {
+    document.getElementById("mySidenav13").style.width = "0";
+    document.getElementById("mySidenav13").style.fontSize = "10px"
+  }
+
+
+  function openNav14() {
+    document.getElementById("mySidenav14").style.width = "210px";
+  }
+  
+  function closeNav14() {
+    document.getElementById("mySidenav14").style.width = "0";
+    document.getElementById("mySidenav14").style.fontSize = "10px"
+  }
+
+
+  function openNav15() {
+    document.getElementById("mySidenav15").style.width = "210px";
+  }
+  
+  function closeNav15() {
+    document.getElementById("mySidenav15").style.width = "0";
+    document.getElementById("mySidenav15").style.fontSize = "10px"
+  }
+
+
+  function openNav16() {
+    document.getElementById("mySidenav16").style.width = "210px";
+  }
+  
+  function closeNav16() {
+    document.getElementById("mySidenav16").style.width = "0";
+    document.getElementById("mySidenav16").style.fontSize = "10px"
+  }
+
+
+  function openNav17() {
+    document.getElementById("mySidenav17").style.width = "210px";
+  }
+  
+  function closeNav17() {
+    document.getElementById("mySidenav17").style.width = "0";
+    document.getElementById("mySidenav17").style.fontSize = "10px"
+  }
